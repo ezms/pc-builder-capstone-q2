4d9421656dba28e25b240d6d8d81fcb17c22ae12
Rafael Schug <rafaelschugbc@gmail.com>
2022-03-09 20:00:22 -0300

    fix: search product

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -8,7 +8,6 @@ import { useModal } from "../../providers/modal";
 import Modal from "../../components/Modal";
 import ModalDetails from "../../components/ModalDetails";
 import { toast } from "react-toastify";
-import { useAuth } from "../../providers/auth";
 
 const Products = () => {
   const history = useHistory();
@@ -25,21 +24,16 @@ const Products = () => {
 
   const [everyProduct, setEveryProduct] = useState([]);
 
-  const { token } = useAuth();
-
-  // const categoryFilter = products[category]?.filter(
-  //   (element) =>
-  //     element.model.toLowerCase().includes(searchInput.toLowerCase()) ||
-  //     element.price.toString().includes(searchInput) ||
-  //     Number(searchInput) <= element.price
-  // );
-
-  // const everyProductFilter = everyProduct.filter(
-  //   (element) =>
-  //     element.model.toLowerCase().includes(searchInput.toLowerCase()) ||
-  //     element.price.toString().includes(searchInput) ||
-  //     Number(searchInput) <= element.price
-  // );
+  useEffect(() => {
+    if (searchInput) {
+      let filtered = products.filter((item) =>
+        item.model.toLowerCase().includes(searchInput.toLowerCase())
+      );
+      setProducts(filtered);
+    } else {
+      setProducts(everyProduct);
+    }
+  }, [searchInput]);
 
   useEffect(() => {
     api
@@ -48,7 +42,7 @@ const Products = () => {
         setProducts(response.data);
         setEveryProduct(response.data);
       })
-      .catch((err) => console.log(err));
+      .catch(() => toast.error("Ops, houve um erro ao carregar produtos!"));
   }, []);
 
   useEffect(() => {
@@ -57,7 +51,9 @@ const Products = () => {
       .then((response) => {
         setCategories(response.data);
       })
-      .catch((err) => console.log(err));
+      .catch((err) =>
+        toast.error("Ops, houve um erro ao carregar categorias!")
+      );
   }, []);
 
   useEffect(() => {
@@ -69,7 +65,11 @@ const Products = () => {
         .then((response) => {
           setProducts(response.data.products);
         })
-        .catch((err) => console.log(err));
+        .catch((err) =>
+          toast.error(
+            "Ops, houve um erro ao carregar os produtos da categoria!"
+          )
+        );
     }
   }, [category]);
 
@@ -112,7 +112,7 @@ const Products = () => {
       )}
       <Container>
         <div id="homeCover">
-          <button onClick={() => history.push("/build")}>Monte seu PC</button>
+          {/* <button onClick={() => history.push("/build")}>Monte seu PC</button> */}
         </div>
         <div id="banner">
           <h3>PRODUTOS</h3>
@@ -130,16 +130,6 @@ const Products = () => {
             id="categories"
           >
             <option value="">Todos</option>
-            {/* <option value="">Todos</option>
-            <option value="drive">Armazenamento</option>
-            <option value="cooler">Cooler</option>
-            <option value="font">Fonte</option>
-            <option value="case">Gabinete</option>
-            <option value="ram">Memória Ram</option>
-            <option value="peripherals">Periféricos</option>
-            <option value="gpu">Placa de vídeo</option>
-            <option value="motherboard">Placa mãe</option>
-            <option value="cpu">Processador</option> */}
             {categories.map((item, index) => {
               return (
                 <option key={index} value={item.category_id}>
@@ -151,103 +141,7 @@ const Products = () => {
         </div>
 
         <div id="products">
-          {
-            // searchInput ? (
-            //   categoryFilter?.length > 0 || everyProductFilter?.length > 0 ? (
-            //     category ? (
-            //       categoryFilter.map((element, index) => (
-            //         <div id="card" key={index}>
-            //           <div id="imageDiv">
-            //             <img src={element.img} alt={element.model} />
-            //           </div>
-            //           <div id="contentDiv">
-            //             <div id="info">
-            //               <h3>{element.model}</h3>
-            //               <p onClick={() => handleOpenModal(element)}>
-            //                 Exibir detalhes <AiOutlinePlusCircle id="plusIcon" />
-            //               </p>
-            //               <h3>
-            //                 {element.price.toLocaleString("pt-BR", {
-            //                   style: "currency",
-            //                   currency: "BRL",
-            //                 })}
-            //               </h3>
-            //             </div>
-            //             <button
-            //               onClick={() => {
-            //                 addToCart(element);
-            //               }}
-            //             >
-            //               Adicionar
-            //             </button>
-            //           </div>
-            //         </div>
-            //       ))
-            //     ) : (
-            //       everyProductFilter.map((element, index) => (
-            //         <div id="card" key={index}>
-            //           <div id="imageDiv">
-            //             <img src={element.img} alt={element.model} />
-            //           </div>
-            //           <div id="contentDiv">
-            //             <div id="info">
-            //               <h3>{element.model}</h3>
-            //               <p onClick={() => handleOpenModal(element)}>
-            //                 Exibir detalhes <AiOutlinePlusCircle id="plusIcon" />
-            //               </p>
-            //               <h3>
-            //                 {element.price.toLocaleString("pt-BR", {
-            //                   style: "currency",
-            //                   currency: "BRL",
-            //                 })}
-            //               </h3>
-            //             </div>
-            //             <button
-            //               onClick={() => {
-            //                 addToCart(element);
-            //               }}
-            //             >
-            //               Adicionar
-            //             </button>
-            //           </div>
-            //         </div>
-            //       ))
-            //     )
-            //   ) : (
-            //     <div id="empty">
-            //       <h3>Nenhum produto encontrado...</h3>
-            //     </div>
-            //   )
-            // ) : category ? (
-            //   products[category]?.map((element, index) => (
-            //     <div id="card" key={index}>
-            //       <div id="imageDiv">
-            //         <img src={element.img} alt={element.model} />
-            //       </div>
-            //       <div id="contentDiv">
-            //         <div id="info">
-            //           <h3>{element.model}</h3>
-            //           <p onClick={() => handleOpenModal(element)}>
-            //             Exibir detalhes <AiOutlinePlusCircle id="plusIcon" />
-            //           </p>
-            //           <h3>
-            //             {element.price.toLocaleString("pt-BR", {
-            //               style: "currency",
-            //               currency: "BRL",
-            //             })}
-            //           </h3>
-            //         </div>
-            //         <button
-            //           onClick={() => {
-            //             addToCart(element);
-            //           }}
-            //         >
-            //           Adicionar
-            //         </button>
-            //       </div>
-            //     </div>
-            //   ))
-            // ) :
+          {products.length > 0 ? (
             products.map((element, index) => (
               <div id="card" key={index}>
                 <div id="imageDiv">
@@ -276,7 +170,15 @@ const Products = () => {
                 </div>
               </div>
             ))
-          }
+          ) : !products.length && searchInput.length > 0 ? (
+            <div id="empty">
+              <h3>Nenhum produto encontrado...</h3>
+            </div>
+          ) : (
+            <div id="empty">
+              <h3>Carregando produtos...</h3>
+            </div>
+          )}
         </div>
       </Container>
     </>
